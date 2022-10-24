ed20c226b22dd54e9b0084a383d35ef797e0461f
vivian <vtharakan@letcargo.com>
2022-10-24 11:26:02 +0300

    Added missed out DBModels

---
+++ InvitationsService/Models/DBModels/DriverInvitations.cs
@@ -0,0 +1,11 @@
+﻿namespace InvitationsService.Models.DBModels
+{
+    public class DriverInvitations
+    {
+        public int DriverInvitationsId { get; set; }
+        public int? VehicleId { get; set; }
+        public int InvitationId { get; set; }
+
+        public virtual Invitations Invitations { get; set; }
+    }
+}
--- InvitationsService/Models/DBModels/InvitationsServiceContext.cs
+++ InvitationsService/Models/DBModels/InvitationsServiceContext.cs
@@ -16,6 +16,7 @@ namespace InvitationsService.Models.DBModels
         public virtual DbSet<Invitations> Invitations { get; set; }
         public virtual DbSet<EmailInvitations> EmailInvitations { get; set; }
         public virtual DbSet<PhoneInvitations> PhoneInvitations { get; set; }
+        public virtual DbSet<DriverInvitations> DriverInvitations { get; set; }
         public virtual DbSet<RegistrationForms> RegistrationForms { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -43,6 +44,13 @@ namespace InvitationsService.Models.DBModels
                     .HasColumnType("enum('email','phone_number')")
                     .HasCharSet("utf8mb4")
                     .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.UserType)
+                  .HasColumnName("user_type")
+                  .HasColumnType("enum('user','driver')")
+                  .HasCharSet("utf8mb4")
+                  .HasCollation("utf8mb4_0900_ai_ci");
+
 
                 entity.Property(e => e.OfficerId).HasColumnName("officer_id");
 
@@ -98,7 +106,25 @@ namespace InvitationsService.Models.DBModels
                     .HasConstraintName("invitations_email_invitations_ibfk_1");
             });
 
+            modelBuilder.Entity<DriverInvitations>(entity =>
+            {
+                entity.HasKey(e => e.DriverInvitationsId).HasName("PRIMARY");
 
+                entity.ToTable("driver_invitations");
+
+                entity.Property(e => e.DriverInvitationsId)
+                .HasColumnName("driver_invitation_id");
+
+
+
+                entity.Property(e => e.InvitationId).HasColumnName("invitation_id");
+                entity.Property(e => e.VehicleId).HasColumnName("vehicle_id");
+
+                entity.HasOne(d => d.Invitations)
+                    .WithOne(p => p.DriverInvitation)
+                    .HasForeignKey<DriverInvitations>(d => d.InvitationId)
+                    .HasConstraintName("invitations_driver_invitations_ibfk_1");
+            });
 
             modelBuilder.Entity<RegistrationForms>(entity =>
             {
