a6260871f4a28a9f8a500009def0f74f0951c33c
vivian <vtharakan@letcargo.com>
2022-10-26 10:27:37 +0300

    added decode applicationId

--- InvitationsService/Repository/InvitationsRepository.cs
+++ InvitationsService/Repository/InvitationsRepository.cs
@@ -179,7 +179,7 @@ namespace InvitationsService.Repository
                 throw new KeyNotFoundException(CommonMessage.RegistrationFormUrlNotFound);
             }
             string token = JsonConvert.DeserializeObject<InvitationTokenResponse>(GetAPI(_dependencies.GenerateInvitationTokenUrl).Content).invitationToken.ToString();
-            if (applicationId == "3")
+            if (Obfuscation.Decode(applicationId) == 3)
                 return registrationForm.Url + Obfuscation.Encode(invitationId) + "&tk=" + token;
             else
                 return registrationForm.Url + "?inv=" + Obfuscation.Encode(invitationId) + "&tk=" + token;
